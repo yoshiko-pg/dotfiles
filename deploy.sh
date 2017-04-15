6d1f7c8bb6756e579533e814eb5cf2b7d646d6b7
maasa <maasa.giss2@gmail.com>
2017-04-16 02:59:35 +0900

    add deploy command

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+cd `dirname $0`
+DOT_DIRECTORY=`pwd`
+
+for f in .??*
+do
+  [[ ${f} = "deploy.sh" ]] && continue
+  [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".gitignore" ]] && continue
+  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+echo -e "\e[32mDeploy dotfiles complete!\e[m"
