9a2992f984bf4c9fc6f80dc261d229d1f556f045
maasa <maasa.giss2@gmail.com>
2017-04-16 23:55:05 +0900

    remove unnecessary line

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ DOT_DIRECTORY=`pwd`
 
 for f in .??*
 do
-  [[ ${f} = "deploy.sh" ]] && continue
   [[ ${f} = ".git" ]] && continue
   [[ ${f} = ".gitignore" ]] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
